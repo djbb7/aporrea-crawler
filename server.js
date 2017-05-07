a894bc3f6feba779778b2673af6a64e4bbedff87
Bruzual Balzan Daniel <bruzuad1@niksula.hut.fi>
2017-05-07 08:51:36 +0300

    Streamline main script

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 var express = require('express');
 
-var crawler = require('./crawler.js');
+var mongoose = require('mongoose')
+
+var crawler = require('./crawler.js')
 
 var reader = require('./reader.js')
 
 var app = express();
 
 var port = 8081;
+
+var db_user = process.env.DBUSER
+
+var db_pass = process.env.DBPASS
+
+var mongoServer = 'mongodb://'+db_user+':'+db_pass+'@ds163698.mlab.com:63698/articles'
+
+mongoose.connect(mongoServer)
 
 app.use('/crawler', crawler)
 
