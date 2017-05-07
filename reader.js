d242f2818fb2328c427baba94084d0b2df40c2ed
Bruzual Balzan Daniel <bruzuad1@niksula.hut.fi>
2017-05-07 10:54:59 +0300

    add timeline endpoint

--- reader.js
+++ reader.js
@@ -10,7 +10,7 @@ mongoose.Promise = global.Promise
 
 router.get('/article/:id', function(req, res){
     Article
-        .findOne({ id: req.params.id })
+        .findOne({ id: req.params.id }, '-_id')
         .then(function(article){
             if(!article){
                 res.sendStatus(404)
@@ -23,7 +23,7 @@ router.get('/article/:id', function(req, res){
 
 router.get('/author/:author', function(req, res){
     Article
-        .find({ author: req.params.author})
+        .find({ author: req.params.author}, '-_id')
         .then(function(articles){
             if(articles.length == 0){
                 res.sendStatus(404)
@@ -39,11 +39,13 @@ router.get('/author/:author', function(req, res){
 })
 
 router.get('/articles/:year/:month/:day', function(req, res){
-    var end = new Date(req.params.year, req.params.month, req.params.day, 23, 59, 59)
-    var start = new Date(req.params.year, req.params.month, req.params.day, 0, 0, 0)
+    //var end = new Date(req.params.year, req.params.month, req.params.day, 23, 59, 59)
+    var end = new Date(req.params.day + '-' + req.params.month + '-' + req.params.year + ' 23:59:59 -04:00')
+    //var start = new Date(req.params.year, req.params.month, req.params.day, 0, 0, 0)
+    var start = new Date(req.params.day + '-' + req.params.month + '-' + req.params.year + ' 00:00:00 -04:00')
 
     Article
-        .find({ datePublished: {"$gte": start, "$lt": end}  })
+        .find({ datePublished: {"$gte": start, "$lt": end}, crawled: true }, '-_id')
         .then(function(articles){
             if(articles.length == 0){
                 res.sendStatus(404)
@@ -58,4 +60,28 @@ router.get('/articles/:year/:month/:day', function(req, res){
         })
 })
 
+router.get('/timeline', function(req, res){
+    var limit = req.query.limit ? parseInt(req.query.limit) : 10
+    var after = req.query.after
+
+    var query = {crawled:true}
+
+    if(after){
+        var date = new Date(after)
+        query.datePublished = {"$lt":date}
+    }
+
+    Article
+        .find(query, '-_id')
+        .limit(limit)
+        .sort('-datePublished')
+        .then(function(articles){
+            res.setHeader('Content-Type', 'application/json')
+            res.send(JSON.stringify(articles))
+        })
+        .catch(function(err){
+            console.log(err)
+            res.sendStatus(500)
+        })
+})
 module.exports = router
